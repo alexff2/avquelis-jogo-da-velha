6fe244c69e0968186a880e79ac4531a0e4a55425
Alexandre Sanva <alex_ff2@hotmail.com>
2021-05-02 23:09:34 -0300

    Movendo components para pasta lib

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
-import { modEasy, bestMove } from './bots'
+import { modEasy, bestMove } from '../lib/bots'
 
 export default function Home() {
   const [ velhaArray, setVelhaArray ] = useState(Array(9).fill("")) //Estado para renderização
